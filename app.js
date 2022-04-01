c148568033e3c8ff160ee944f0d2dbb9443d3662
Doug <douglashorville@gmail.com>
2022-04-01 09:37:56 -0400

    Add database, insert and render first objects

--- app.js
+++ app.js
@@ -1,27 +1,67 @@
 //jshint esversion:6
 
 const express = require('express');
-const date = require(__dirname + '/date.js');
+const mongoose = require('mongoose');
 
 const app = express();
+const init = new Date();
 
 app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
-
-app.get('/'', function(req, res) {
-
-const day = date.getDate();
-
-  res.render('list', {listTitle: day, newListItems: items});
-
+mongoose.connect('mongodb://localhost:27017/todolistDB',
+{ useNewUrlParser: true }, (err) => {
+  if(!err) {
+    console.log("Connected to MongoDB.");
+    const time = new Date() - init;
+    console.log(`Connection took ${time} ms.`);
+  } else {
+    console.log(err.message);
+  }
 });
 
-app.post('/'', function(req, res){
+const itemsSchema = new mongoose.Schema({
+  name: String
+});
+
+const Item = mongoose.model('Item', itemsSchema);
+
+const item1 = new Item ({
+  name: "Welcome to your todolist!"
+});
+
+const item2 = new Item ({
+  name: "Hit the + button to add a new item."
+});
+
+const item3 = new Item ({
+  name: "<-- Hit this to delete an item."
+});
+
+const defaultItems = [item1, item2, item3];
+
+// Item.insertMany(defaultItems, (err) => {
+//   if (!err) {
+//     console.log("Successfully saved defaultItems to todolistDB.");
+//   } else {
+//     console.log(err.message);
+//   }
+// });
+
+app.get('/', function(req, res) {
+
+  Item.find({}, (err, foundItems) => {
+    if(!err) {
+      res.render('list', {listTitle: "Today", newListItems: foundItems});
+    } else {
+      console.log(err);
+    };
+  });
+});
+
+app.post('/', function(req, res){
 
   const item = req.body.newItem;
 
@@ -30,7 +70,7 @@ app.post('/'', function(req, res){
     res.redirect('/work');
   } else {
     items.push(item);
-    res.redirect('/'');
+    res.redirect('/');
   }
 });
 
@@ -49,5 +89,5 @@ app.get('/about', function(req, res){
 });
 
 app.listen(3000, function() {
-  console.log("Server started on port 3000");
+  console.log("Server started on port 3000.");
 });
